50629f34c826dfcd71d795f1ea9d21d4d6497765
ketanthakkar <ketanthakkar.156@gmail.com>
2018-11-24 14:09:21 +0100

    removed extra file

--- index.js
+++
@@ -1,5 +0,0 @@
-var element = React.createElement('h1', {  }, 'Hello, world!');
-const root = React.createElement(
-    "div", {}, element);
- ReactDOM.render(root, document.getElementById("root"));
-//ReactDOM.render(element, document.getElementById('root'));
