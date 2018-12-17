7cdec5b096ecf580fdf1a80fcf31e832fbac31f4
Ketan Thakkar <44302674+ketanthakkar@users.noreply.github.com>
2018-12-17 23:17:06 +0100

    Update Footer.test.js

--- test/Footer.test.js
+++ test/Footer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Footer from "../src/components/Footer";
 
-describe('should renders Footer correctly', () => {
+describe('should render Footer correctly', () => {
   const tree = mount(<Footer />);
   test('Snapshot test', () => {
     expect(tree).toMatchSnapshot();
