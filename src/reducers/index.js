71e417af6ddd22eeda6c5178d9514142ff139349
Ketan Thakkar <44302674+ketanthakkar@users.noreply.github.com>
2019-01-06 18:56:07 +0100

    Revert "Revert "Hometask5: Flux + Redux""

---
+++ src/reducers/index.js
@@ -0,0 +1,55 @@
+import { REQUEST_MOVIES, RECEIVE_MOVIES, FILTER_MOVIES, FILTER_MOVIES_BY, SORT_MOVIES } from '../actions';
+import { combineReducers } from 'redux';
+
+const movies = (state = {movieData: []}, action) => {
+
+    switch(action.type) {
+        case REQUEST_MOVIES:
+            return { ...state }
+
+        case RECEIVE_MOVIES:
+            return {
+                ...state,
+                movieData: action.movies
+            }
+            
+        default:
+            return state;
+    }
+}
+
+const search = (state = {searchStr: "", searchby: "title"}, action) => {
+    switch(action.type) {
+        case FILTER_MOVIES:
+            return { 
+                ...state,
+                searchStr: action.searchStr
+             }
+
+        case FILTER_MOVIES_BY:
+            return { 
+                ...state,
+                searchby: action.searchby
+             }
+
+        default:
+            return state;     
+    }
+}
+
+const sortBy = (state = {sortby: "release_date"}, action) => {
+    switch(action.type) {
+        case SORT_MOVIES:
+            return { 
+                ...state,
+                sortby: action.sortby
+             }
+             
+        default:
+            return state;     
+    }
+}
+
+export default combineReducers({
+    movies, search, sortBy
+});
